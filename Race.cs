52de49a020d3d2f416681b79a5d0bd27b668d564
NivorBUD <via4eslav.dubrovin@yandex.ru>
2023-05-10 15:18:23 +0500

    фикс 3.2

--- Race.cs
+++ Race.cs
@@ -200,7 +200,7 @@ public partial class Race : Form
 
     private void MakeDir(DirectoryInfo imagesDirectory = null)
     {
-        var way = @"..\..\..\GameImages\";
+        var way = @"..\..\GameImages\";
         if (imagesDirectory == null)
             imagesDirectory = new DirectoryInfo(way);
         foreach (var e in imagesDirectory.GetFiles("*.png"))
